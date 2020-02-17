c56f881269f7f1b3519d3f3e7ae2320fd2a0c78c
Ismail <ismail32cse@gmail.com>
2020-02-17 16:44:12 +0600

    first commit

---
+++ app/Http/Controllers/frontend/PagesController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\frontend;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class PagesController extends Controller
+{
+
+    public function index()
+    {
+       return view('frontend.pages.index');
+    }
+   
+}
